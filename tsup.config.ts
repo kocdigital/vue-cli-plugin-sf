fe54a3b99f435f471de3a3aa76e318f0698f63d0
Asım Tahir KARAKUŞ <asim.tahirkarakus@ext.kocdigital.com>
2026-08-03 17:02:48 +0300

    feat: add `sf-pages-entry` page entrypoint for pages codegen

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     entry: {
         index: 'src/index.ts',
         config: 'src/config.ts',
+        'sf-pages-entry': 'src/sf-pages-entry.ts',
     },
     clean: true,
     format: ['cjs'],
